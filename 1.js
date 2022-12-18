db5d00663e47b18321e71015b8fc98271e47e9ae
MargaritaM12 <108419145+MargaritaM12@users.noreply.github.com>
2022-12-18 23:27:55 +0300

    First commit

---
+++ 1.js
@@ -0,0 +1,14 @@
+const string = document.querySelector ('input');
+const button = document.querySelector('button');
+
+button.addEventListener ('click',()=>
+{
+    document.querySelector('input').value;
+    const array = string.split(" "); 
+    console.log('array');
+})
+
+
+
+
+
